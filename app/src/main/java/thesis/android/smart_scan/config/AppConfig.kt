75c0b43987aa999cb4f5b0cec981c1ce1dbc1340
vuhau190904 <vuhau190904@gmail.com>
2026-05-03 18:19:17 +0700

    feat: change mediapipe-object-extract to mlkit--object-extract

--- app/src/main/java/thesis/android/smart_scan/config/AppConfig.kt
+++ app/src/main/java/thesis/android/smart_scan/config/AppConfig.kt
@@ -4,6 +4,5 @@ import java.util.Locale
 
 data class AppConfig(
     val userLanguage: String = Locale.getDefault().language,
-    val objectDetectorScoreThreshold: Float = 0.5f,
-    val objectDetectorDisplayNamesLocales: List<String> = listOf(Locale.getDefault().toLanguageTag())
+    val objectDetectorScoreThreshold: Float = 0.8f,
 )
